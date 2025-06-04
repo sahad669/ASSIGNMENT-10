7b036610a15a3f3b463ce86657ef69f9cadf2029
sahad <sahadmosco@gmail.com>
2025-06-04 09:04:53 +0400

    added serch input area for seach products name

--- src/Pages/Productlist.jsx
+++ src/Pages/Productlist.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { useState,useEffect } from 'react'
-import Card from '../Components/Card';
-import axios from 'axios';
+import React from "react";
+import { useState, useEffect } from "react";
+import Card from "../Components/Card";
+import axios from "axios";
 
 const Productlist = () => {
-   const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const getProducts = async () => {
     const response = await axios.get("./Products.json");
@@ -17,10 +17,17 @@ const Productlist = () => {
       setFilteredProducts(products);
       return;
     }
-    const filteredUsers = products.filter((item) => {
+    const filteredProducts = products.filter((item) => {
       return item.category == e.target.value;
     });
-    setFilteredProducts(filteredUsers);
+    setFilteredProducts(filteredProducts);
+  };
+
+  const handleInput = (e) => {
+    const filteredProducts = products.filter((item) =>
+      item.title.toLowerCase().includes(e.target.value)
+    );
+    setFilteredProducts(filteredProducts);
   };
 
   useEffect(() => {
@@ -29,23 +36,45 @@ const Productlist = () => {
 
   return (
     <div className="px-4 min-h-screen">
-      <div className="container mx-auto px-4 flex flex-col md:flex-row justify-end items-center gap-4 m-5 mb-10">
-  <label htmlFor="" className="text-lg font-medium text-gray-700">
-    Filter Category
-  </label>
-  <select
-    id="category-filter"
-    onChange={handleCategory}
-    className="w-60 md:w-72 border border-gray-300 rounded-md p-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-  >
-    <option value="All">All</option>
-    <option value="men's clothing">men's clothing</option>
-    <option value="jewelery">jewelery</option>
-    <option value="electronics">electronics</option>
-    <option value="women's clothing">women's clothing</option>
-  </select>
-</div>
+      <div className="container mx-auto px-4 my-6">
+        <div className="flex flex-col md:flex-row items-center justify-between gap-4">
+          <div className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-4">
+            <label
+              htmlFor="search"
+              className="text-lg font-medium text-gray-700"
+            >
+              Search
+            </label>
+            <input
+              id="search"
+              onInput={handleInput}
+              className="w-60 md:w-72 border border-gray-300 rounded-md p-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              type="search"
+              placeholder="Search product name"
+            />
+          </div>
 
+          <div className="flex flex-col md:flex-row items-start md:items-center gap-2 md:gap-4">
+            <label
+              htmlFor="category-filter"
+              className="text-lg font-medium text-gray-700"
+            >
+              Filter Category
+            </label>
+            <select
+              id="category-filter"
+              onChange={handleCategory}
+              className="w-60 md:w-72 border border-gray-300 rounded-md p-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            >
+              <option value="All">All</option>
+              <option value="men's clothing">Men's Clothing</option>
+              <option value="jewelery">Jewelery</option>
+              <option value="electronics">Electronics</option>
+              <option value="women's clothing">Women's Clothing</option>
+            </select>
+          </div>
+        </div>
+      </div>
 
       <div className="mx-auto max-w-7xl pb-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
@@ -56,6 +85,6 @@ const Productlist = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Productlist
+export default Productlist;
